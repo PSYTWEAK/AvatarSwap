00a47cfa8a30f51c09a3ccf053924429df22b886
PSYTWEAK <psy@promise.exchange>
2022-11-11 17:24:38 +0000

    tweaks

--- scripts/collectionData.js
+++ scripts/collectionData.js
@@ -1,13 +1,7 @@
-
-
-
 export const collectionData = [
-    {
-        collectionAddress: "0x91E51B92a2EfEA89bF1B6f66ad719737264724bE",
-        ranges: 
-            [900, // if ID lower than 900, it's an Avo Cato
-            1049, // if ID lower than 1049, it's a Hot Dog
-            2238], // if ID lower than 2238, it's a Mouse au Chocolat
-        avatarTypes: ["Avo Cato","Hot Dog","Mouse au Chocolat"]
-    },
-]
+  {
+    collectionAddress: "0x622d8FeA4603BA9EdAF1084B407052D8b0A9bed7",
+    ranges: [250000],
+    avatarTypes: ["Argentina"],
+  },
+];
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,30 @@
 import { ethers } from "hardhat";
 // @ts-ignore
-import { collectionData } from "./collectionData";
+const collectionData = [
+  {
+    collectionAddress: "0x91E51B92a2EfEA89bF1B6f66ad719737264724bE",
+    ranges: [
+      900, // if ID lower than 900, it's an Avo Cato
+      1049, // if ID lower than 1049, it's a Hot Dog
+      2238,
+    ], // if ID lower than 2238, it's a Mouse au Chocolat
+    avatarTypes: ["Avo Cato", "Hot Dog", "Mouse au Chocolat"],
+  },
+  {
+    collectionAddress: "0x622d8FeA4603BA9EdAF1084B407052D8b0A9bed7",
+    ranges: [250000],
+    avatarTypes: ["Argentina"],
+  },
+];
 
 async function main() {
   const AvatarSwap = await ethers.getContractFactory("AvatarSwap");
 
-  let avatarSwap = await AvatarSwap.deploy("0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619");
-
-  await avatarSwap.deployed();
+  const avatarSwap = await AvatarSwap.attach("0xd0870Dd658e5e75b091B23464DBD55De45f8d181");
 
   console.log("AvatarSwap deployed to:", avatarSwap.address);
 
-  // add collections
-  for (let i = 0; i < collectionData.length; i++) {
-    avatarSwap.addCollection(collectionData.collectionAddress, collectionData.ranges, collectionData.avatarTypes);
-  }
+  await avatarSwap.addCollection(collectionData[1].collectionAddress, collectionData[1].ranges, collectionData[1].avatarTypes);
 
   console.log("Collections added to AvatarSwap");
 }
